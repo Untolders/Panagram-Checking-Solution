4dc4a44d0ceb81a94b203d2e818307242eb54036
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-02-01 10:05:08 +0530

    Create Panagram-Checking-Solutionc.pp

---
+++ Panagram-Checking-Solutionc.pp
@@ -0,0 +1,100 @@
+###########################################################################################################################################  Question  #####################################################################################################
+
+Given a string s check if it is "Panagram" or not.
+
+A "Panagram" is a sentence containing every letter in the English Alphabet.
+
+Example 1:
+
+Input:
+s = "Bawds jog, flick quartz, vex nymph"
+Output: 
+1
+Explanation: 
+In the given input, there
+are all the letters of the English
+alphabet. Hence, the output is 1.
+Example 2:
+
+Input:
+s = "sdfs"
+Output: 
+0
+Explanation: 
+In the given input, there
+aren't all the letters present in the
+English alphabet. Hence, the output
+is 0.
+Your Task:
+You do not have to take any input or print anything. You need to complete the function checkPangram() that takes a string as a parameter and returns true if the string is a Panagram, or else it returns false.
+
+Expected Time Complexity: O( |s| )
+Expected Auxiliary Space: O(1)
+|s| denotes the length of the input string.
+
+Constraints:
+1 ≤ |s| ≤ 104
+Both Uppercase & Lowercase are considerable
+
+
+
+
+############################################################################################################################################  Solution  ####################################################################################################
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to check if a string is Pangram or not.
+    bool checkPangram (string s) {
+        // your code here
+        map<char,int>mp;
+        int n=s.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(s[i]<=90 && s[i]>=65)
+            s[i]=s[i]+32;
+            
+            mp[s[i]]++;
+            
+            if(mp[s[i]]==1 && s[i]>=97 && s[i]<=122)
+            count++;
+        }
+        if(count==26)
+        return 1;
+        
+        return 0;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+// Driver Program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--){
+        string s;
+        getline(cin, s);
+        Solution obj;
+        if (obj.checkPangram(s) == true)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+    return(0);
+}
+
+// } Driver Code Ends
